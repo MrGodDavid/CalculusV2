1ceedffa1ec0638f15c5853395cee314c1a343f0
MrGodDavid <davidbodongliu@gmail.com>
2026-07-18 22:35:31 -0400

    Update 8-5. Minor updates.

--- src/test/java/Vector3dTest.java
+++ src/test/java/Vector3dTest.java
@@ -9,13 +9,15 @@ public class Vector3dTest {
 
     @Test
     void testAddition() {
+        // case 1
         Vector3d a = new Vector3d(1, 2, 3);
         Vector3d b = new Vector3d(4, 5, 6);
-
         Vector3d result = a.add(b);
-        assertEquals(5, result.getX(), DELTA);
-        assertEquals(7, result.getY(), DELTA);
-        assertEquals(9, result.getZ(), DELTA);
+        assertEquals(5d, result.getX(), DELTA);
+        assertEquals(7d, result.getY(), DELTA);
+        assertEquals(9d, result.getZ(), DELTA);
+
+
     }
 
     @Test
@@ -24,9 +26,9 @@ public class Vector3dTest {
         Vector3d b = new Vector3d(4, 5, 6);
 
         Vector3d result = a.subtract(b);
-        assertEquals(-3, result.getX(), DELTA);
-        assertEquals(-3, result.getY(), DELTA);
-        assertEquals(-3, result.getZ(), DELTA);
+        assertEquals(-3d, result.getX(), DELTA);
+        assertEquals(-3d, result.getY(), DELTA);
+        assertEquals(-3d, result.getZ(), DELTA);
     }
 
     @Test
@@ -34,16 +36,16 @@ public class Vector3dTest {
         Vector3d v = new Vector3d(1, -2, 3);
         Vector3d result = v.scale(2);
 
-        assertEquals(2, result.getX(), DELTA);
-        assertEquals(-4, result.getY(), DELTA);
-        assertEquals(6, result.getZ(), DELTA);
+        assertEquals(2d, result.getX(), DELTA);
+        assertEquals(-4d, result.getY(), DELTA);
+        assertEquals(6d, result.getZ(), DELTA);
     }
 
     @Test
     void testMagnitude() {
         Vector3d v = new Vector3d(3, 4, 12);
 
-        assertEquals(13, v.length(), DELTA);
+        assertEquals(13d, v.length(), DELTA);
     }
 
     @Test
@@ -61,7 +63,7 @@ public class Vector3dTest {
         Vector3d v = new Vector3d(1, 2, 3);
         Vector3d u = new Vector3d(4, -5, 6);
 
-        assertEquals(12, v.dot_product(u), DELTA);
+        assertEquals(12d, v.dot_product(u), DELTA);
     }
 
     @Test
@@ -70,9 +72,9 @@ public class Vector3dTest {
         Vector3d b = new Vector3d(10, 25, 20);
 
         Vector3d result = a.crossProduct(b, false);
-        assertEquals(105, result.getX(), DELTA);
-        assertEquals(-50, result.getY(), DELTA);
-        assertEquals(10, result.getZ(), DELTA);
+        assertEquals(105d, result.getX(), DELTA);
+        assertEquals(-50d, result.getY(), DELTA);
+        assertEquals(10d, result.getZ(), DELTA);
     }
 
 }
